fe4003b228cbe0c6e2c6d37b8fcf98adace428f6
Willian <willianx64@gmail.com>
2022-12-19 11:57:12 -0300

    feat: Rotas de vehicles+listAllPublishedVehicles rota

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import "express-async-errors";
 import express from "express";
 import handlerErrorMiddleware from "./middlewares/handler.middleware";
 import cors from "cors";
+import vehiclesRoutes from "./routes/vehicles.routes";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/vehicles", vehiclesRoutes)
 
 app.use(handlerErrorMiddleware);
 
