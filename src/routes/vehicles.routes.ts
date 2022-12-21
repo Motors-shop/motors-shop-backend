adc7f382f59edde3ec68896d796d253b414e3952
Willian <willianx64@gmail.com>
2022-12-21 12:00:41 -0300

    feat: Rota de listSessionVehicles

--- src/routes/vehicles.routes.ts
+++ src/routes/vehicles.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { deleteVehicleController } from "../controllers/vehicles/deleteVehicle.controller";
 import { listAllPublishedVehiclesController } from "../controllers/vehicles/listAllPublishedVehicles.controller";
+import { listSessionVehiclesController } from "../controllers/vehicles/listSessionVehicles.controller";
 import { listUserPublishedVehiclesController } from "../controllers/vehicles/listUserPublishedVehicles.controller";
 import { retrievePublishedVehicleController } from "../controllers/vehicles/retrievePublishedVehicle.controller";
 
@@ -10,6 +11,7 @@ const vehiclesRoutes = Router();
 vehiclesRoutes.get("", listAllPublishedVehiclesController);
 vehiclesRoutes.get("/:id", retrievePublishedVehicleController);
 vehiclesRoutes.get("/user/:id", listUserPublishedVehiclesController);
+vehiclesRoutes.get("/session/user", listSessionVehiclesController)
 // vehiclesRoutes.patch("/:id", );
 vehiclesRoutes.delete("/:id", deleteVehicleController);
 
